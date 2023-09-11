a0f69e433348ab1cdf62db4655358b42c9e58dc4
fturannn <turanfurkannn@gmail.com>
2023-09-11 16:22:41 +0300

    Proje oluşturuldu ve test edildi

--- Not_Ortalamasi/src/NotOrtalamasi.java
+++ Not_Ortalamasi/src/NotOrtalamasi.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class NotOrtalamasi {
+import java.util.Scanner;
+
+public class NotOrtalamasi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int matematik, fizik, kimya, turkce, tarih, muzik;
+
+        System.out.print("Matematik notunu giriniz: ");
+        matematik = input.nextInt();
+
+        System.out.print("Fizik notunu giriniz: ");
+        fizik = input.nextInt();
+
+        System.out.print("Kimya notunu giriniz: ");
+        kimya = input.nextInt();
+
+        System.out.print("Türkçe notunu giriniz: ");
+        turkce = input.nextInt();
+
+        System.out.print("Tarih notunu giriniz: ");
+        tarih = input.nextInt();
+
+        System.out.print("Müzik notunu giriniz: ");
+        muzik = input.nextInt();
+
+        int toplam = matematik + fizik + kimya + turkce + tarih + muzik;
+        double ortalama = toplam / 6.0;
+        System.out.println("Girilen derslerin ortalaması: " + ortalama);
+
+        String kaldiGecti = (ortalama >= 60) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println(kaldiGecti);
+    }
 }
+
